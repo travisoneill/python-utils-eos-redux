683972a8538be40a2e83ef0d40375e73dd22e6c4
travis.h.oneill@gmail.com <travis.h.oneill@gmail.com>
2016-10-15 13:41:36 -0700

    v 0.0.1

--- eos_python_utils/setup.py
+++ eos_python_utils/setup.py
@@ -2,10 +2,10 @@ from setuptools import setup
 
 setup(
     name='eos_python_utils',
-    version='0.0.0',
+    version='0.0.1',
+    url='https://github.com/travisoneill/python-utils-eos-redux',
     description='python route handling for eos-redux',
-    author='Travis O\'Neill',
+    author='Travis Oneill',
     author_email='travis.h.oneill@gmail.com',
-    scripts=['eos_python_utils'],
     zip_safe=False
 )
